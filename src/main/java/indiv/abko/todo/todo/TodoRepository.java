12c0a99c5c3605d5506fbe83c34972f2f4e0e535
abko <abko121115@gmail.com>
2025-07-29 11:49:45 +0900

    feat: - 작성자명을 포함하며 수정일 기준 내림차순 조회하는 메서드 - 수정일 기준 내림차순 하는 조회 메서드

--- src/main/java/indiv/abko/todo/todo/TodoRepository.java
+++ src/main/java/indiv/abko/todo/todo/TodoRepository.java
@@ -1,7 +1,11 @@
 package indiv.abko.todo.todo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
-    
+    List<Todo> findByAuthorContainingOrderByModifiedAtDesc(String author);
+
+    List<Todo> findByAuthorOrderByModifiedAtDesc(String author);
 }
