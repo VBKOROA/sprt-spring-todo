1a40248cc47e9e9f41f500f16e3391b271eb446d
abko <abko121115@gmail.com>
2025-07-29 11:07:54 +0900

    docs: 주석 추가

--- src/main/java/indiv/abko/todo/common/dto/ApiResponse.java
+++ src/main/java/indiv/abko/todo/common/dto/ApiResponse.java
@@ -3,7 +3,7 @@ package indiv.abko.todo.common.dto;
 import org.springframework.http.HttpStatus;
 
 public record ApiResponse<T> (
-    HttpStatus status,
+    HttpStatus status, // OK, CREATED 등의 문자열로 직렬화됨
     String message,
     T data
 ) {}
