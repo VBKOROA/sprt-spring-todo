6c93499ba1c3f936576b7f719dfd9b7bfa4fef17
abko <abko121115@gmail.com>
2025-07-29 11:40:41 +0900

    fix: auditing을 위한 필수 어노테이션을 추가

--- src/main/java/indiv/abko/todo/TodoApplication.java
+++ src/main/java/indiv/abko/todo/TodoApplication.java
@@ -2,8 +2,10 @@ package indiv.abko.todo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing // 이거 필수임!!
 public class TodoApplication {
 
 	public static void main(String[] args) {
