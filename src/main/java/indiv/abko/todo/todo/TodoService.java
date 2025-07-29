027042c28fa582ed92705da2b408f5579dbb1797
abko <abko121115@gmail.com>
2025-07-29 11:28:24 +0900

    feat: 일정 생성 기능 구현

--- src/main/java/indiv/abko/todo/todo/TodoService.java
+++ src/main/java/indiv/abko/todo/todo/TodoService.java
@@ -1,11 +1,38 @@
 package indiv.abko.todo.todo;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import indiv.abko.todo.common.util.Encrypt;
+import indiv.abko.todo.todo.dto.CreateTodoReq;
+import indiv.abko.todo.todo.dto.CreateTodoResp;
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class TodoService {
     private final TodoRepository todoRepo;
+    private final Encrypt encrypt;
+
+    @Transactional
+    public CreateTodoResp create(CreateTodoReq todoReq) {
+        Todo todo = Todo.builder()
+            .title(todoReq.title())
+            .content(todoReq.content())
+            .author(todoReq.author())
+            .password(encrypt.hash(todoReq.password()))
+            .build();
+        
+        var result = todoRepo.save(todo);
+
+        var response = new CreateTodoResp(
+            result.getTitle(),
+            result.getContent(),
+            result.getAuthor(),
+            result.getCreatedAt(),
+            result.getModifiedAt()
+        );
+
+        return response;
+    } 
 }
