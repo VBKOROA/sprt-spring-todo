1209dc0208f2de50fd63182ab7b6c9c0e41359cb
abko <abko121115@gmail.com>
2025-07-29 11:00:23 +0900

    feat: jpa auditing을 위한 BaseTimeEntity 공통 추상 클래스 작성

---
+++ src/main/java/indiv/abko/todo/common/entity/BaseTimeEntity.java
@@ -0,0 +1,24 @@
+package indiv.abko.todo.common.entity;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+
+@Getter
+@MappedSuperclass // 상속 받는 entity가 아래의 필드들을 컬럼으로 매핑
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedAt;
+}
