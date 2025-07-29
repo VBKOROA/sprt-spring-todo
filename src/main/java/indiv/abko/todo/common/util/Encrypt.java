faa7e0ab9c94464f1513a5211fdaf77712f8e330
abko <abko121115@gmail.com>
2025-07-29 11:26:04 +0900

    fix: 접근 제어자 변경

--- src/main/java/indiv/abko/todo/common/util/Encrypt.java
+++ src/main/java/indiv/abko/todo/common/util/Encrypt.java
@@ -5,11 +5,11 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Encrypt {
-    private String hash(String str) {
+    public String hash(String str) {
         return BCrypt.hashpw(str, BCrypt.gensalt());
     }
 
-    private boolean compareHash(String original, String hashed) {
+    public boolean compareHash(String original, String hashed) {
         return BCrypt.checkpw(original, hashed);
     }
 }
