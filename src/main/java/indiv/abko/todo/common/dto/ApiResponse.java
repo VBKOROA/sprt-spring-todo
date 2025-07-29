f2461689d231c655059152e28c68c0430c510b37
abko <abko121115@gmail.com>
2025-07-29 15:12:24 +0900

    feat: ApiResponse 클래스에 Ok 정적 팩토리 메서드 추가

--- src/main/java/indiv/abko/todo/common/dto/ApiResponse.java
+++ src/main/java/indiv/abko/todo/common/dto/ApiResponse.java
@@ -6,4 +6,8 @@ public record ApiResponse<T> (
     HttpStatus status, // OK, CREATED 등의 문자열로 직렬화됨
     String message,
     T data
-) {}
+) {
+    public static <T> ApiResponse<T> Ok(T data) {
+        return new ApiResponse<>(HttpStatus.OK, "", data);
+    }
+}
