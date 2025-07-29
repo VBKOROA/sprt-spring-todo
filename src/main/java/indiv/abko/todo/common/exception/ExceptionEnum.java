bb14169d934868199f52235a74d39af1dcf44d23
abko <abko121115@gmail.com>
2025-07-29 14:38:17 +0900

    feat: UNKNOWN_ERROR 추가 및 예외 처리 개선

--- src/main/java/indiv/abko/todo/common/exception/ExceptionEnum.java
+++ src/main/java/indiv/abko/todo/common/exception/ExceptionEnum.java
@@ -3,6 +3,7 @@ package indiv.abko.todo.common.exception;
 import org.springframework.http.HttpStatus;
 
 public enum ExceptionEnum {
+    UNKNOWN_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "원인 불명의 오류가 발생했습니다."),
     TODO_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 ID의 일정이 없습니다.");
 
     private final HttpStatus status;
