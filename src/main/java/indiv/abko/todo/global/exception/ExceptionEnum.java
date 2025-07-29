5f749b39e73564c1d166105b759511db0ffa6e6b
abko <abko121115@gmail.com>
2025-07-29 19:24:21 +0900

    feat: ExceptionEnum에 TODO_UPDATE_UNAUTHORIZED 추가

--- src/main/java/indiv/abko/todo/global/exception/ExceptionEnum.java
+++ src/main/java/indiv/abko/todo/global/exception/ExceptionEnum.java
@@ -7,7 +7,8 @@ import lombok.Getter;
 @Getter
 public enum ExceptionEnum {
     UNKNOWN_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "원인 불명의 오류가 발생했습니다."),
-    TODO_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 ID의 일정이 없습니다.");
+    TODO_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 ID의 일정이 없습니다."),
+    TODO_UPDATE_UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "수정할 권한이 없습니다.");
 
     private final HttpStatus status;
     private final String message;
