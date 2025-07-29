9f8a02327689fab3e779625a57be820e798ebf05
abko <abko121115@gmail.com>
2025-07-29 14:08:00 +0900

    fix: 비밀번호 동등성을 위한 salt값 고정

--- src/main/java/indiv/abko/todo/common/util/Encrypt.java
+++ src/main/java/indiv/abko/todo/common/util/Encrypt.java
@@ -6,10 +6,12 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Encrypt {
+    private final String salt = BCrypt.gensalt();
+
     // @Named로 이름을 붙여서 "명시적으로 호출할 때만" 사용하도록 제한
     @Named("hashPassword")
     public String hash(String str) {
-        return BCrypt.hashpw(str, BCrypt.gensalt());
+        return BCrypt.hashpw(str, salt);
     }
 
     public boolean compareHash(String original, String hashed) {
