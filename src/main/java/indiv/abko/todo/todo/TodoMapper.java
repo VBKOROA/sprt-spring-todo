cb6f3945de7362a3420a3c9ea4897ee5a93f2b8b
abko <abko121115@gmail.com>
2025-07-29 12:55:33 +0900

    fix: 패스워드 해싱

--- src/main/java/indiv/abko/todo/todo/TodoMapper.java
+++ src/main/java/indiv/abko/todo/todo/TodoMapper.java
@@ -1,13 +1,16 @@
 package indiv.abko.todo.todo;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
+import indiv.abko.todo.common.util.Encrypt;
 import indiv.abko.todo.todo.dto.CreateTodoReq;
 import indiv.abko.todo.todo.dto.CreateTodoResp;
 import indiv.abko.todo.todo.dto.GetTodosResp;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = Encrypt.class)
 public interface TodoMapper {
+    @Mapping(target = "password", expression = "java(encrypt.hash(todoReq.password()))")
     Todo toTodo(CreateTodoReq req);
 
     GetTodosResp.TodoDto toTodoDto(Todo todo);
