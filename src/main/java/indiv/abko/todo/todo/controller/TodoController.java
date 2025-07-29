b0a362fd58fbe90fe6ebc903653750c44db6fabb
abko <abko121115@gmail.com>
2025-07-29 19:59:38 +0900

    feat: 일정 삭제 기능 추가

--- src/main/java/indiv/abko/todo/todo/controller/TodoController.java
+++ src/main/java/indiv/abko/todo/todo/controller/TodoController.java
@@ -1,6 +1,7 @@
 package indiv.abko.todo.todo.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +18,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -51,5 +53,12 @@ public class TodoController {
     @PatchMapping("/{id}")
     public ApiResponse<TodoResp> updateTodo(@PathVariable("id") Long id, @RequestBody TodoUpdateReq updateReq) {
         return ApiResponse.ok(todoService.updateTodo(id, updateReq));
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public ApiResponse<Void> updateTodo(@PathVariable("id") Long id, @RequestParam("password") String password) {
+        todoService.deleteTodo(id, password);
+        return ApiResponse.noContent();
     }
 }
