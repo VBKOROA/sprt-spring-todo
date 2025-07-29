a25d3a8abd8f18d302a7c38cdd845c4a97c7e8cf
abko <abko121115@gmail.com>
2025-07-29 15:17:24 +0900

    feat: ApiResponse 클래스에 error 정적 팩토리 메서드 추가

--- src/main/java/indiv/abko/todo/common/dto/ApiResponse.java
+++ src/main/java/indiv/abko/todo/common/dto/ApiResponse.java
@@ -2,6 +2,8 @@ package indiv.abko.todo.common.dto;
 
 import org.springframework.http.HttpStatus;
 
+import indiv.abko.todo.common.exception.ExceptionEnum;
+
 public record ApiResponse<T> (
     HttpStatus status, // OK, CREATED 등의 문자열로 직렬화됨
     String message,
@@ -9,5 +11,9 @@ public record ApiResponse<T> (
 ) {
     public static <T> ApiResponse<T> Ok(T data) {
         return new ApiResponse<>(HttpStatus.OK, "", data);
+    }
+
+    public static <T> ApiResponse<T> error(ExceptionEnum e, T data) {
+        return new ApiResponse<T>(e.getStatus(), e.getMessage(), data);
     }
 }
