2b39a430d5fc34147bfb00333479d5f0b9223b95
abko <abko121115@gmail.com>
2025-07-30 14:45:30 +0900

    feat: TodoUpdateReq에서 title과 author 필드에 OptionalNotBlank 어노테이션 추가

--- src/main/java/indiv/abko/todo/todo/dto/TodoUpdateReq.java
+++ src/main/java/indiv/abko/todo/todo/dto/TodoUpdateReq.java
@@ -1,12 +1,15 @@
 package indiv.abko.todo.todo.dto;
 
+import indiv.abko.todo.global.validation.OptionalNotBlank;
 import indiv.abko.todo.todo.validation.ValidTodoTitle;
 import jakarta.validation.constraints.NotBlank;
 
 public record TodoUpdateReq(
     @ValidTodoTitle
+    @OptionalNotBlank
     String title,
 
+    @OptionalNotBlank
     String author,
 
     @NotBlank
