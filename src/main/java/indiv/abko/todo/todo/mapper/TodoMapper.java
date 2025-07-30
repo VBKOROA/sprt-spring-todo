2c6968d29925f149b717f0c9212fea554763d216
abko <abko121115@gmail.com>
2025-07-30 12:32:06 +0900

    feat: TodoMapper에 TodoWithCommentsResp 변환 메서드 추가

--- src/main/java/indiv/abko/todo/todo/mapper/TodoMapper.java
+++ src/main/java/indiv/abko/todo/todo/mapper/TodoMapper.java
@@ -6,6 +6,7 @@ import org.mapstruct.Mapping;
 import indiv.abko.todo.global.util.Encrypt;
 import indiv.abko.todo.todo.dto.TodoCreateReq;
 import indiv.abko.todo.todo.dto.TodoResp;
+import indiv.abko.todo.todo.dto.TodoWithCommentsResp;
 import indiv.abko.todo.todo.entity.Todo;
 
 @Mapper(componentModel = "spring", uses = Encrypt.class)
@@ -15,4 +16,6 @@ public interface TodoMapper {
     Todo toTodo(TodoCreateReq req);
 
     TodoResp toTodoResp(Todo todo);
+    
+    TodoWithCommentsResp toTodoWithCommentsResp(Todo todo);
 }
