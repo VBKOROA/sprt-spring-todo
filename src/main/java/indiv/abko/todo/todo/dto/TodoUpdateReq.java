f8d9a3ed836bb046d9b4aade06f5550ba5e37551
abko <abko121115@gmail.com>
2025-07-30 13:52:01 +0900

    feat: TodoUpdateReq 필드에 유효성 검증 추가

--- src/main/java/indiv/abko/todo/todo/dto/TodoUpdateReq.java
+++ src/main/java/indiv/abko/todo/todo/dto/TodoUpdateReq.java
@@ -1,8 +1,17 @@
 package indiv.abko.todo.todo.dto;
 
+import org.hibernate.validator.constraints.Length;
+import jakarta.validation.constraints.NotBlank;
+
 public record TodoUpdateReq(
+    @Length(max = 30)
+    @NotBlank
     String title,
+
+    @NotBlank
     String author,
+
+    @NotBlank
     String password
 ) {
 
