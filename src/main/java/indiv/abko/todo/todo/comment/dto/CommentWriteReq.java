e567c49f65245022ff97a6ef5697da79883dcf5d
abko <abko121115@gmail.com>
2025-07-30 13:51:45 +0900

    feat: CommentWriteReq 필드에 유효성 검증 추가

--- src/main/java/indiv/abko/todo/todo/comment/dto/CommentWriteReq.java
+++ src/main/java/indiv/abko/todo/todo/comment/dto/CommentWriteReq.java
@@ -1,8 +1,17 @@
 package indiv.abko.todo.todo.comment.dto;
 
+import org.hibernate.validator.constraints.Length;
+import jakarta.validation.constraints.NotBlank;
+
 public record CommentWriteReq(
+    @Length(max=100)
+    @NotBlank
     String content,
+
+    @NotBlank
     String author,
+
+    @NotBlank
     String password
 ) {
 }
