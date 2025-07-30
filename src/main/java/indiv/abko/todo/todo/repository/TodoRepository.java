261e60eec58afcd7d47cc6b75a0aa7e2ae57cc59
abko <abko121115@gmail.com>
2025-07-30 22:33:02 +0900

    fix: TodoRepository에서 JpaSpecificationExecutor 상속으로 수정

--- src/main/java/indiv/abko/todo/todo/repository/TodoRepository.java
+++ src/main/java/indiv/abko/todo/todo/repository/TodoRepository.java
@@ -1,13 +1,13 @@
 package indiv.abko.todo.todo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import indiv.abko.todo.todo.entity.Todo;
 
 import java.util.List;
 
 
-public interface TodoRepository extends JpaRepository<Todo, Long>, TodoQRepository {
+public interface TodoRepository extends JpaRepository<Todo, Long>, JpaSpecificationExecutor<Todo> {
     List<Todo> findByAuthorOrderByModifiedAtDesc(String author);
 
     List<Todo> findByOrderByModifiedAtDesc();
