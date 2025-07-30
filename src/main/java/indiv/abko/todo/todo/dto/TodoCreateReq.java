69ade1607b8aab9e8404a92bd447e17043b310a1
abko <abko121115@gmail.com>
2025-07-30 13:51:51 +0900

    feat: TodoCreateReq 필드에 유효성 검증 추가

--- src/main/java/indiv/abko/todo/todo/dto/TodoCreateReq.java
+++ src/main/java/indiv/abko/todo/todo/dto/TodoCreateReq.java
@@ -1,9 +1,21 @@
 package indiv.abko.todo.todo.dto;
 
+import org.hibernate.validator.constraints.Length;
+import jakarta.validation.constraints.NotBlank;
+
 public record TodoCreateReq(
+    @Length(max = 30)
+    @NotBlank
     String title,
+
+    @Length(max = 200)
+    @NotBlank
     String content,
+
+    @NotBlank
     String author,
+
+    @NotBlank
     String password
 ) {
 
