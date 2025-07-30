ec82ca6f31bdb13b247cc82e4f702c39a6c58e50
abko <abko121115@gmail.com>
2025-07-31 00:09:21 +0900

    fix: authorLike 메서드 수정<br> feat: titleLike, contentLike 메서드 추가

--- src/main/java/indiv/abko/todo/todo/repository/TodoSpecification.java
+++ src/main/java/indiv/abko/todo/todo/repository/TodoSpecification.java
@@ -5,13 +5,37 @@ import org.springframework.util.StringUtils;
 import indiv.abko.todo.todo.entity.Todo;
 
 public class TodoSpecification {
-    public static Specification<Todo> authorEquals(final String author) {
+    public static Specification<Todo> authorLike(final String author) {
         return (root, query, builder) -> {
             if(StringUtils.hasText(author) == false) {
                 // 항상 true 반환
                 return builder.conjunction();
             }
-            return builder.equal(root.get(Todo.Fields.author), author);
+            return builder.like(root.get(Todo.Fields.author), toPatternString(author));
         };
+    }
+
+    public static Specification<Todo> titleLike(final String title) {
+        return (root, query, builder) -> {
+            if(StringUtils.hasText(title) == false) {
+                // 항상 true 반환
+                return builder.conjunction();
+            }
+            return builder.equal(root.get(Todo.Fields.title), toPatternString(title));
+        };
+    }
+
+    public static Specification<Todo> contentLike(final String content) {
+        return (root, query, builder) -> {
+            if(StringUtils.hasText(content) == false) {
+                // 항상 true 반환
+                return builder.conjunction();
+            }
+            return builder.equal(root.get(Todo.Fields.content), toPatternString(content));
+        };
+    }
+
+    private static String toPatternString(String str) {
+        return "%" + str + "%";
     }
 }
