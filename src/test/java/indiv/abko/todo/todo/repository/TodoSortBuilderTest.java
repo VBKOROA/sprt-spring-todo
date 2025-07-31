86e378185d7e09d47a407fb4c255e7d20492706a
abko <abko121115@gmail.com>
2025-07-31 23:36:37 +0900

    test: TodoSortBuilderTest 클래스 추가로 정렬 기능 테스트 구현

---
+++ src/test/java/indiv/abko/todo/todo/repository/TodoSortBuilderTest.java
@@ -0,0 +1,24 @@
+package indiv.abko.todo.todo.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.data.domain.Sort.Order;
+import indiv.abko.todo.todo.entity.Todo;
+
+public class TodoSortBuilderTest {
+    @Test
+    @DisplayName("정렬 스트링 파싱 성공")
+    void 유효한정렬스트링이면_sort를반환한다() {
+        // Given
+        final String orderString = "id_asc";
+        
+        // When
+        var sort = TodoSortBuilder.buildWith(orderString);
+        var order = sort.getOrderFor(Todo.Fields.id.toString());
+
+        // Than
+        assertThat(order).isNotNull()
+            .isEqualTo(Order.asc(Todo.Fields.id.toString()));
+    } 
+}
