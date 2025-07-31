0051e7f694e6b042449457f06a51a18aa03d2d5b
abko <abko121115@gmail.com>
2025-07-31 22:45:08 +0900

    fix: TodoSpecification -> TodoSpecBuilder 로직 이동 및 빌더 패턴 도입

--- src/main/java/indiv/abko/todo/todo/repository/TodoSpecBuilder.java
+++ src/main/java/indiv/abko/todo/todo/repository/TodoSpecBuilder.java
@@ -1,7 +1,7 @@
 package indiv.abko.todo.todo.repository;
 
 import org.springframework.data.jpa.domain.Specification;
-import indiv.abko.todo.todo.dto.TodoSearchCondition;
+import org.springframework.util.StringUtils;
 import indiv.abko.todo.todo.entity.Todo;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
@@ -9,11 +9,47 @@ import lombok.NoArgsConstructor;
 // 외부에서의 생성 차단
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class TodoSpecBuilder {
-    public static Specification<Todo> buildWith(TodoSearchCondition condition) {
-        Specification<Todo> spec = Specification.unrestricted();
-        spec = spec.and(TodoSpecification.authorLike(condition.author()));
-        spec = spec.and(TodoSpecification.titleLike(condition.title()));
-        spec = spec.and(TodoSpecification.contentLike(condition.content()));
+    private Specification<Todo> spec = Specification.unrestricted();
+
+    // public static Specification<Todo> buildWith(TodoSearchCondition condition) {
+    //     Specification<Todo> spec = Specification.unrestricted();
+    //     spec = spec.and(TodoSpecification.authorLike(condition.author()));
+    //     spec = spec.and(TodoSpecification.titleLike(condition.title()));
+    //     spec = spec.and(TodoSpecification.contentLike(condition.content()));
+    //     return spec;
+    // }
+
+    public static TodoSpecBuilder builder() {
+        return new TodoSpecBuilder();
+    }
+
+    public TodoSpecBuilder authorLike(final String author) {
+        addLikeSpecification(author, Todo.Fields.author);
+        return this;
+    }
+
+    public TodoSpecBuilder titleLike(final String title) {
+        addLikeSpecification(title, Todo.Fields.title);
+        return this;
+    }
+
+    public TodoSpecBuilder contentLike(final String content) {
+        addLikeSpecification(content, Todo.Fields.content);
+        return this;
+    }
+
+    public Specification<Todo> build() {
         return spec;
+    }
+
+    private void addLikeSpecification(final String value, final Todo.Fields field) {
+        if(StringUtils.hasText(value) == true) {
+            spec = spec.and((root, query, cb) 
+                -> cb.like(root.get(field.toString()), toPatternString(value)));
+        }
+    }
+
+    private String toPatternString(final String str) {
+        return "%" + str + "%";
     }
 }
