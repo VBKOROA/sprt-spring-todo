cdb6a2bace69846a8d0683ca40c4cf27e378e831
abko <abko121115@gmail.com>
2025-08-01 00:13:53 +0900

    refactor: 메서드 파라미터에 final 부착

--- src/main/java/indiv/abko/todo/global/util/Encrypt.java
+++ src/main/java/indiv/abko/todo/global/util/Encrypt.java
@@ -8,11 +8,11 @@ import org.springframework.stereotype.Component;
 public class Encrypt {
     // @Named로 이름을 붙여서 "명시적으로 호출할 때만" 사용하도록 제한
     @Named("hashPassword")
-    public String hash(String str) {
+    public String hash(final String str) {
         return BCrypt.hashpw(str, BCrypt.gensalt());
     }
 
-    public boolean isHashEqual(String original, String hashed) {
+    public boolean isHashEqual(final String original, final String hashed) {
         return BCrypt.checkpw(original, hashed);
     }
 }
