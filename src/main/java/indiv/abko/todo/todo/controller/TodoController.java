9473c605ccc1dec241d7530522ccf0ddf311f0a8
abko <abko121115@gmail.com>
2025-08-01 13:27:43 +0900

    docs: updateTodo에 인증 실패에 대한 Swagger 작성

--- src/main/java/indiv/abko/todo/todo/controller/TodoController.java
+++ src/main/java/indiv/abko/todo/todo/controller/TodoController.java
@@ -101,6 +101,11 @@ public class TodoController {
                 schema = @Schema(implementation = ApiResp.class),
                 examples = @ExampleObject(value = "{\"status\":\"BAD_REQUEST\",\"message\":\"요청이 잘못되었습니다.\",\"data\":null}")
             )),
+        @ApiResponse(responseCode = "401", description = "인증 실패",
+            content = @Content(
+                schema = @Schema(implementation = ApiResp.class),
+                examples = @ExampleObject(value = "{\"status\":\"UNAUTHORIZED\",\"message\":\"인증에 실패하였습니다.\",\"data\":null}")
+            )),
         @ApiResponse(responseCode = "404", description = "Todo를 찾을 수 없음",
             content = @Content(
                 schema = @Schema(implementation = ApiResp.class),
