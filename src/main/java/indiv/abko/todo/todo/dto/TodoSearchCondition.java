3170920e3324ea802c9effbb5bb0ee6de122ff5a
abko <abko121115@gmail.com>
2025-08-01 13:14:56 +0900

    docs: TodoSearchCondition에 swagger 어노테이션 추가

--- src/main/java/indiv/abko/todo/todo/dto/TodoSearchCondition.java
+++ src/main/java/indiv/abko/todo/todo/dto/TodoSearchCondition.java
@@ -1,9 +1,16 @@
 package indiv.abko.todo.todo.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "Todo 검색 조건 DTO")
 public record TodoSearchCondition(
+    @Schema(description = "정렬 기준", example = "createdAt_desc", requiredMode = Schema.RequiredMode.REQUIRED)
     String orderBy,
+    @Schema(description = "제목", example = "Todo 제목")
     String title,
+    @Schema(description = "내용", example = "Todo 내용")
     String content,
+    @Schema(description = "작성자", example = "Todo 작성자")
     String author
 ) {
 }
