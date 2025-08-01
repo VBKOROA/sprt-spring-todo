7defd92b3380bf1d5462c8623b79c346aece9a1b
abko <abko121115@gmail.com>
2025-08-01 21:49:51 +0900

    test: TodoControllerInterTest에 제목이 없는 경우의 Todo 생성 실패 테스트 추가

--- src/test/java/indiv/abko/todo/todo/controller/TodoControllerInterTest.java
+++ src/test/java/indiv/abko/todo/todo/controller/TodoControllerInterTest.java
@@ -10,11 +10,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@Transactional
 @ActiveProfiles("test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class TodoControllerInterTest {
@@ -58,6 +60,30 @@ public class TodoControllerInterTest {
             assertThat(map.get("title")).isEqualTo("테스트");
             assertThat(map.get("content")).isEqualTo("테스트");
             assertThat(map.get("author")).isEqualTo("테스트");
+        }
+
+        @Test
+        @DisplayName("Todo 생성 실패 테스트 - 제목이 없는 경우")
+        void 제목이없으면_Todo생성이실패한다() {
+            // Given
+            TodoCreateReq todoCreateReq = TodoCreateReq.builder()
+                    .title(null)
+                    .content("테스트")
+                    .author("테스트")
+                    .password("테스트")
+                    .build();
+
+            // When
+            var reqResponse = testRestTemplate.postForEntity("/api/v1/todos", todoCreateReq, ApiResp.class);
+
+            // Then
+            assertThat(reqResponse.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+            assertThat(reqResponse.getBody()).isNotNull();
+
+            var body =  reqResponse.getBody();
+
+            assertThat(body.status()).isEqualTo(HttpStatus.BAD_REQUEST);
+            assertThat(body.data()).isNull();
         }
     }
 }
