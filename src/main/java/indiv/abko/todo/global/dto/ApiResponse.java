662051831d76e8ec7f3ad420a8e0e83da8fa1d0d
abko <abko121115@gmail.com>
2025-08-01 12:49:39 +0900

    docs: ApiResponse에 대한 @Schema 어노테이션 추가

--- src/main/java/indiv/abko/todo/global/dto/ApiResponse.java
+++ src/main/java/indiv/abko/todo/global/dto/ApiResponse.java
@@ -3,10 +3,15 @@ package indiv.abko.todo.global.dto;
 import org.springframework.http.HttpStatus;
 
 import indiv.abko.todo.global.exception.ExceptionEnum;
+import io.swagger.v3.oas.annotations.media.Schema;
 
+@Schema(description = "API 응답 DTO")
 public record ApiResponse<T> (
+    @Schema(description = "HTTP 상태 코드", example = "OK")
     HttpStatus status, // OK, CREATED 등의 문자열로 직렬화됨
+    @Schema(description = "메시지", example = "요청이 잘못되었습니다.")
     String message,
+    @Schema(description = "응답 데이터")
     T data
 ) {
     public static <T> ApiResponse<T> ok(T data) {
