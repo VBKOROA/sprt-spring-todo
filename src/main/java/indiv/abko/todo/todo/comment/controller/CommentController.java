6675bd449279d23d7bb2c3fa22eb9ae17c63945e
abko <abko121115@gmail.com>
2025-08-01 16:06:36 +0900

    docs: 댓글 작성 API에 Swagger 문서화

--- src/main/java/indiv/abko/todo/todo/comment/controller/CommentController.java
+++ src/main/java/indiv/abko/todo/todo/comment/controller/CommentController.java
@@ -6,6 +6,9 @@ import indiv.abko.todo.global.dto.ApiResp;
 import indiv.abko.todo.todo.comment.dto.CommentResp;
 import indiv.abko.todo.todo.comment.dto.CommentWriteReq;
 import indiv.abko.todo.todo.comment.service.CommentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,12 +19,19 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RestController
 @RequestMapping("/api/v1/todos/{todoId}/comments")
 @RequiredArgsConstructor
+@Tag(name = "Comment API", description = "할일 관리 시스템의 댓글 관련 API")
 public class CommentController {
     private final CommentService commentService;
 
     @PostMapping("")
-    public ApiResp<CommentResp> writeComment(@PathVariable("todoId") Long todoId,
-            @RequestBody @Valid CommentWriteReq req) {
+    @Operation(summary = "댓글 작성", description = "특정 할일에 대한 댓글을 작성함.")
+    public ApiResp<CommentResp> writeComment(
+        @PathVariable("todoId")
+        @Parameter(name = "todoId", description = "할일 ID")
+        Long todoId,
+        @RequestBody
+        @Valid
+        CommentWriteReq req) {
         return ApiResp.ok(commentService.createComment(todoId, req));
     }
 }
