124721272a5bf38cb6db78b5103636c33b7ba022
abko <abko121115@gmail.com>
2025-08-01 16:17:17 +0900

    test: TodoSortBuilderTest에 정렬 스트링 파싱 실패 테스트 추가

--- src/test/java/indiv/abko/todo/todo/repository/TodoSortBuilderTest.java
+++ src/test/java/indiv/abko/todo/todo/repository/TodoSortBuilderTest.java
@@ -3,6 +3,8 @@ package indiv.abko.todo.todo.repository;
 import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.domain.Sort.Order;
 import indiv.abko.todo.todo.entity.Todo;
 
@@ -20,5 +22,22 @@ public class TodoSortBuilderTest {
         // Then
         assertThat(order).isNotNull()
             .isEqualTo(Order.asc(Todo.Fields.id.toString()));
-    } 
+    }
+
+    @Test
+    @DisplayName("정렬 스트링 파싱 실패")
+    void 유효하지않은정렬스트링이면_기본Sort를반환한다() {
+        // Given
+        final String orderString = "invalid_order";
+        final Sort sortShouldBe = Sort.by(Direction.DESC, Todo.Fields.modifiedAt.toString());
+        final Sort sortShouldntBe = Sort.by(Direction.ASC, Todo.Fields.id.toString());
+
+        // When
+        var sort = TodoSortBuilder.buildWith(orderString);
+
+        // Then
+        assertThat(sort).isNotNull()
+            .isEqualTo(sortShouldBe)
+            .isNotEqualTo(sortShouldntBe);
+    }
 }
