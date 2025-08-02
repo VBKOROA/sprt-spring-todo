414effbc26cc094612cea4a5fbb35c0f212af1f8
abko <abko121115@gmail.com>
2025-08-02 12:17:49 +0900

    feat: TodoMapper에서 TodoTitle 변환 로직 추가

--- src/main/java/indiv/abko/todo/todo/mapper/TodoMapper.java
+++ src/main/java/indiv/abko/todo/todo/mapper/TodoMapper.java
@@ -3,6 +3,7 @@ package indiv.abko.todo.todo.mapper;
 import indiv.abko.todo.todo.comment.mapper.CommentMapper;
 import indiv.abko.todo.todo.dto.TodoWithCommentsResp;
 import indiv.abko.todo.todo.entity.vo.TodoPassword;
+import indiv.abko.todo.todo.entity.vo.TodoTitle;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
@@ -13,13 +14,25 @@ import indiv.abko.todo.todo.entity.Todo;
 
 @Mapper(componentModel = "spring", uses = {CommentMapper.class})
 public interface TodoMapper {
-    default TodoPassword toPassword(String rawPassword, Encrypt encrypt) {
+    default TodoPassword toTodoPassword(String rawPassword, Encrypt encrypt) {
+        if (rawPassword == null) {return null;}
         return new TodoPassword(rawPassword).encrypted(encrypt);
     }
 
-    @Mapping(target = "password", expression = "java(toPassword(req.password(), encrypt))")
+    default TodoTitle toTodoTitle(String title) {
+        if (title == null) {return null;}
+        return new TodoTitle(title);
+    }
+
+    default String toTitle(TodoTitle title) {
+        return title.getTitle();
+    }
+
+    @Mapping(target = "password", expression = "java(toTodoPassword(req.password(), encrypt))")
+    @Mapping(target = "title", expression = "java(toTodoTitle(req.title(), encrypt))")
     Todo toTodo(TodoCreateReq req, Encrypt encrypt);
 
+    @Mapping(target = "title", expression = "java(toTitle(todo.getTitle()))")
     TodoResp toTodoResp(Todo todo);
 
     @Mapping(target = "todo", source = "todo")
