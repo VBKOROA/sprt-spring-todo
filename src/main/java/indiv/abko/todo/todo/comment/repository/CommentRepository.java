94a8105c79aec7fdae2d7c68ae74189691e30cc1
abko <abko121115@gmail.com>
2025-08-02 17:30:08 +0900

    refactor: CommentRepository 삭제 / 사용하지 않음

--- src/main/java/indiv/abko/todo/todo/comment/repository/CommentRepository.java
+++
@@ -1,8 +0,0 @@
-package indiv.abko.todo.todo.comment.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import indiv.abko.todo.todo.comment.entity.Comment;
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-
-}
