36e3e0488760b6bc5702440c6624754831761627
abko <abko121115@gmail.com>
2025-08-04 13:40:21 +0900

    refactor: OptionalNotBlankValidator.isValid 를 simplify

--- src/main/java/indiv/abko/todo/todo/presentation/validation/OptionalNotBlankValidator.java
+++ src/main/java/indiv/abko/todo/todo/presentation/validation/OptionalNotBlankValidator.java
@@ -6,13 +6,6 @@ import jakarta.validation.ConstraintValidatorContext;
 public class OptionalNotBlankValidator implements ConstraintValidator<OptionalNotBlank, String> {
     @Override
     public boolean isValid(String value, ConstraintValidatorContext context) {
-        if (isExistButBlank(value)) {
-            return false;
-        }
-        return true;
-    }
-
-    private boolean isExistButBlank(String value) {
-        return value != null && value.isBlank();
+        return value != null && value.isBlank() == false;
     }
 }
