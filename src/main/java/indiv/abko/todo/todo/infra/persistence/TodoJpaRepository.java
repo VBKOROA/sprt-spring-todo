123bb7d2ab1cab73ea70ce4594a3ca7fa8b26ebf
abko <abko121115@gmail.com>
2025-08-04 09:23:35 +0900

    feat: TodoJpaRepository 생성

---
+++ src/main/java/indiv/abko/todo/todo/infra/persistence/TodoJpaRepository.java
@@ -0,0 +1,16 @@
+package indiv.abko.todo.todo.infra.persistence;
+
+import indiv.abko.todo.todo.domain.Todo;
+import indiv.abko.todo.todo.infra.persistence.entity.TodoJpaEntity;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface TodoJpaRepository {
+    @Query("""
+        SELECT t FROM Todo t LEFT JOIN FETCH t.comments c
+        WHERE t.id = :id
+    """)
+    Optional<TodoJpaEntity> findByIdWithComments(@Param("id") Long id);
+}
