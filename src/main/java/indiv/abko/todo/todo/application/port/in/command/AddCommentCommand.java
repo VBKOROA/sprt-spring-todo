bbf352034b617de6f07ce63ddf9d88e34ca0241e
abko <abko121115@gmail.com>
2025-08-05 14:20:41 +0900

    refactor: Add @Builder annotation to command records for improved instantiation

--- src/main/java/indiv/abko/todo/todo/application/port/in/command/AddCommentCommand.java
+++ src/main/java/indiv/abko/todo/todo/application/port/in/command/AddCommentCommand.java
@@ -1,5 +1,8 @@
 package indiv.abko.todo.todo.application.port.in.command;
 
+import lombok.Builder;
+
+@Builder
 public record AddCommentCommand(
     Long todoId,
     String content,
--- src/main/java/indiv/abko/todo/todo/application/port/in/command/UpdateTodoCommand.java
+++ src/main/java/indiv/abko/todo/todo/application/port/in/command/UpdateTodoCommand.java
@@ -1,5 +1,8 @@
 package indiv.abko.todo.todo.application.port.in.command;
 
+import lombok.Builder;
+
+@Builder
 public record UpdateTodoCommand(
     Long id,
     String title,
